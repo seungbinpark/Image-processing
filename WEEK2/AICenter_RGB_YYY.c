bdc9082dd1f7b6756b72c4a871de375ceb558ee3
seungbinpark <70265096+seungbinpark@users.noreply.github.com>
2024-03-27 21:47:14 +0900

    Add files via upload

--- WEEK2/AICenter_RGB_YYY.c
+++ WEEK2/AICenter_RGB_YYY.c
@@ -12,8 +12,8 @@ int main(int argc, char* argv[]) {
 	fread(&bmpFile, sizeof(BITMAPFILEHEADER), 1, inputFile);
 	fread(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, inputFile);
 
-	int width = bmpInfo.biWidth;	//rgb í¬í•¨
-	int height = bmpInfo.biHeight;		
+	int width = bmpInfo.biWidth;	//rgb Æ÷ÇÔ
+	int height = bmpInfo.biHeight;
 	int size = bmpInfo.biSizeImage;
 	int bitCnt = bmpInfo.biBitCount;
 	int stride = (((bitCnt / 8) * width) + 3) / 4 * 4;
@@ -27,37 +27,43 @@ int main(int argc, char* argv[]) {
 	fread(inputImg, sizeof(unsigned char), size, inputFile);
 
 
-	float Y,Cb,Cr;
+	float Y, Cb, Cr, R, G, B;
+	unsigned char* Yd = NULL;
+
+	Yd = (unsigned char*)calloc(height * width, sizeof(unsigned char));
 
 
-	//input imgëŠ” ê³ ë ¤í• ê²Œ ë§ìŒ
-	// rgbë¥¼ yë¡œ ë°”ê¿”ì„œ ì˜ìƒ ì¢Œí‘œë¥¼
-	// outputImg ì‘ì—…
+	//input img´Â °í·ÁÇÒ°Ô ¸¹À½
+	// rgb¸¦ y·Î ¹Ù²ã¼­ ¿µ»ó ÁÂÇ¥¸¦
+	// outputImg ÀÛ¾÷
 
-	//for (int j = 0;j < height;j++) {
-	//	for (int i = 0;i < width;i++) {
-	//		Y = 0.299 * inputImg[j * stride + 3 * i + 2] + 0.587 * inputImg[j * stride + 3 * i + 1] + 0.114 * inputImg[j * stride + 3 * i + 0];
-	//		Cb = -0.169 * inputImg[j * stride + 3 * i + 2] + -0.331 * inputImg[j * stride + 3 * i + 1] + 0.500 * inputImg[j * stride + 3 * i + 0];
-	//		Cr = 0.500 * inputImg[j * stride + 3 * i + 2] - 0.419 * inputImg[j * stride + 3 * i + 1] - 0.0813 * inputImg[j * stride + 3 * i + 0];
-	//	}
-	//}
+	for (int j = 0;j < height;j++) {
+		for (int i = 0;i < width;i++) {
+			Y = 0.299 * inputImg[j * stride + 3 * i + 2] + 0.587 * inputImg[j * stride + 3 * i + 1] + 0.114 * inputImg[j * stride + 3 * i + 0];
+			Yd[j * width + i] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
+			//Cb = -0.169 * inputImg[j * stride + 3 * i + 2] + -0.331 * inputImg[j * stride + 3 * i + 1] + 0.500 * inputImg[j * stride + 3 * i + 0];
+			//Cr = 0.500 * inputImg[j * stride + 3 * i + 2] - 0.419 * inputImg[j * stride + 3 * i + 1] - 0.0813 * inputImg[j * stride + 3 * i + 0];
+		}
+	}
 
 
 
 	for (int j = 0;j < height;j++) {
 		for (int i = 0;i < width;i++) {
-			Y = 0.299 * inputImg[j * stride + 3 * i + 2] + 0.587 * inputImg[j * stride + 3 * i + 1] + 0.114 * inputImg[j * stride + 3 * i + 0];
-			Cb = -0.169 * inputImg[j * stride + 3 * i + 2] + -0.331 * inputImg[j * stride + 3 * i + 1] + 0.500 * inputImg[j * stride + 3 * i + 0];
+			//Y = 0.299 * inputImg[j * stride + 3 * i + 2] + 0.587 * inputImg[j * stride + 3 * i + 1] + 0.114 * inputImg[j * stride + 3 * i + 0];
+			Cb = -0.169 * inputImg[j * stride + 3 * i + 2] - 0.331 * inputImg[j * stride + 3 * i + 1] + 0.500 * inputImg[j * stride + 3 * i + 0];
 			Cr = 0.500 * inputImg[j * stride + 3 * i + 2] - 0.419 * inputImg[j * stride + 3 * i + 1] - 0.0813 * inputImg[j * stride + 3 * i + 0];
 
-			outputImg[j * stride + 3 * i + 2] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
-			outputImg[j * stride + 3 * i + 1] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
-			outputImg[j * stride + 3 * i + 0] = (unsigned char)(Y > 255 ? 255 : (Y < 0 ? 0 : Y));
+			//R = Yd[j * width + i] + 1.402 * Cr;
+			//R = (unsigned char)(R > 255 ? 255 : (R < 0 ? 0 : R));
+			//G = Yd[j * width + i] - 0.714 * Cr - 0.344 * Cb;
+			//G = (unsigned char)(G > 255 ? 255 : (G < 0 ? 0 : G));
+			//B = Yd[j * width + i] + 1.772 * Cb;
+			//B = (unsigned char)(B > 255 ? 255 : (B < 0 ? 0 : B));
 
-
-			outputImg[j * stride + 3 * i + 0] = Y;
-			outputImg[j * stride + 3 * i + 1] = Y;
-			outputImg[j * stride + 3 * i + 2] = Y;
+			outputImg[j * stride + 3 * i + 2] = Yd[j * width + i];
+			outputImg[j * stride + 3 * i + 1] = Yd[j * width + i];
+			outputImg[j * stride + 3 * i + 0] = Yd[j * width + i];
 
 		}
 	}
@@ -68,6 +74,7 @@ int main(int argc, char* argv[]) {
 	fwrite(&bmpInfo, sizeof(BITMAPINFOHEADER), 1, outputFile);
 	fwrite(outputImg, sizeof(unsigned char), size, outputFile);
 
+	free(Yd);
 	free(outputImg);
 	free(inputImg);
 	fclose(inputFile);
